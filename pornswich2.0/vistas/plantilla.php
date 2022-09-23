c08bc290ddc9b655eff407aa19885b42f3f64e6d
Iago Prieto Lamas <iagoprietolamas@gmail.com>
2022-09-23 02:44:59 +0200

    formater

--- pornswich2.0/vistas/plantilla.php
+++ pornswich2.0/vistas/plantilla.php
@@ -1,71 +1,74 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>practica</title>
-    
-    <!-- #font awesome #-->
-    <script src="https://kit.fontawesome.com/8877593f2a.js" crossorigin="anonymous"></script>
-    
-    <!--# boostrap #-->
-    <!-- Latest compiled and minified CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Latest compiled JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script> 
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>practica</title>
+
+  <!-- #font awesome #-->
+  <script src="https://kit.fontawesome.com/8877593f2a.js" crossorigin="anonymous"></script>
+
+  <!--# boostrap #-->
+  <!-- Latest compiled and minified CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Latest compiled JavaScript -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
 
 
 </head>
+
 <body>
-    
 
-    <!-- logo -->
-    <div class="container-fluid">
-        <h3 class="text-center py-3">
-            LOGO
-        </h3>
+
+  <!-- logo -->
+  <div class="container-fluid">
+    <h3 class="text-center py-3">
+      LOGO
+    </h3>
+  </div>
+
+  <div class="container-fluid bg-light">
+    <div class="container">
+      <ul class="nav nav-justified py2 nav-pills">
+        <li class="nav-item">
+          <a class="nav-link active" href="registro.html">HOME</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="ingreso.html">INGRESO</a>
+        </li>
+        <li class="nav-item">
+
+          <a class="nav-link" href="registro.html">REGISTRO</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">SALIR</a>
+        </li>
+      </ul>
     </div>
-
-    <div class="container-fluid bg-light">
-        <div class="container">
-          <ul class="nav nav-justified py2 nav-pills">
-            <li class="nav-item">
-              <a class="nav-link active" href="registro.html">HOME</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="ingreso.html">INGRESO</a>
-            </li>
-            <li class="nav-item"> 
-
-              <a class="nav-link" href="registro.html">REGISTRO</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">SALIR</a>
-            </li>
-          </ul>
-        </div>
-      </div>
+  </div>
 
 
 
-    </div>
+  </div>
 
 
 
-<div class="container-fluid">
-  <div class="container py-5">
-    <?php
+  <div class="container-fluid">
+    <div class="container py-5">
+      <?php
 
       include "paginas/inicio.php"
-    
-    ?>
+
+      ?>
+    </div>
   </div>
-</div>
 
 
 </body>
+
 </html>
